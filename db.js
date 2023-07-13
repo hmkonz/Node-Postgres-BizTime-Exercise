3f2abcb2419c2618a1a086e03e65aec4e761ccf5
Heather Konz <hmkonz@yahoo.com>
2023-07-13 11:12:05 -0600

    updated files

--- db.js
+++ db.js
@@ -6,16 +6,21 @@ let DB_URL;
 // if running in test "mode", use biztime_test db
 // Need to create both databases!
 
-if (process.env.NODE_ENV === "test") {
-  DB_URL = "biztime_test";
-} else {
-  DB_URL = "biztime";
-}
+// if (process.env.NODE_ENV === "test") {
+//   DB_URL = "biztime_test";
+// } else {
+//   DB_URL = "biztime";
+// }
 
-let db = new Client({
-  host: "/var/run/postgresql",
-  database: DB_URL,
-});
+
+const db = new Client({
+  connectionString: `postgresql:///${DB_URL}`
+  });
+
+// let db = new Client({
+//   host: "/var/run/postgresql",
+//   database: DB_URL,
+// });
 
 db.connect();
 
